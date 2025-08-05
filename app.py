99b6c1380b25b8fd893e88758f7a3ce8e48d1ab5
dudetech-web <dude46122@gmail.com>
2025-08-05 22:36:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, session
+from flask import Flask, render_template, request, redirect, url_for, session, jsonify
 import sqlite3
 import math
 from datetime import datetime
